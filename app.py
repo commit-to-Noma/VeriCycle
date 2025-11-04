bf474d123c496ef341145dd3d106c6a49a86618a
NomathembaNcube <nomathembancube2005@gmail.com>
2025-11-05 00:08:57 +0200

    feat: add QR code generation functionality for collector dashboard

--- app.py
+++ app.py
@@ -19,6 +19,47 @@ def collector_dashboard():
 def center_dashboard():
     return render_template('center.html')
 
-# This line makes the app run when you execute the python file
+# (The application will be started at the bottom after all routes are defined)
+
+
+# --- ADD THIS NEW CODE AT THE BOTTOM OF APP.PY ---
+
+# We need to import the new tools
+import qrcode
+import io
+from flask import send_file
+
+# This new route is specifically for creating the QR code
+@app.route('/generate-qr')
+def generate_qr():
+    
+    # This is the data we will put in the QR code.
+    # This is your TEST collector ID from your .env file!
+    # LATER, we will change this to be the ID of the logged-in user.
+    collector_id = "0.0.7191569"
+    
+    # Generate the QR code in memory
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(collector_id)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    
+    # Save the image to a "memory buffer" instead of a file
+    img_io = io.BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    
+    # Send the image back to the browser
+    return send_file(img_io, mimetype='image/png')
+
+# --- END OF NEW CODE ---
+
+# Start the Flask development server when this file is executed directly
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
